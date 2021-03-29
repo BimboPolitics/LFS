63cf64f540a969e566b014ce348be4bac5e43f09
BimboPolitics <79517398+BimboPolitics@users.noreply.github.com>
2021-03-29 04:49:48 -0500

    changed script for 10.1

--- lfs/stage-2/stage-2.1/file.sh
+++ lfs/stage-2/stage-2.1/file.sh
@@ -8,9 +8,16 @@ PRGNAME="file"
 source "$(pwd)/check_environment.sh"                  || exit 1
 source "$(pwd)/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
-./configure       \
-    --prefix=/usr \
-    --host="${LFS_TGT}" || exit 1
+mkdir build
+pushd build
+  ../configure --disable-bzlib      \
+               --disable-libseccomp \
+               --disable-xzlib      \
+               --disable-zlib
+  make
+popd || exit 1
 
-make || make -j1 || exit 1
+./configure --prefix=/usr --host="${LFS_TGT}" --build=$(./config.guess) || exit 1
+
+make FILE_COMPILE=$(pwd)/build/src/file || make -j1 || exit 1
 make install DESTDIR="${LFS}"
